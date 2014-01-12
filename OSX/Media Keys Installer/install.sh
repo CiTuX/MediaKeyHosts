ac7b4787a71484fdcc2920338143c4619a12e79d
Michael Feldstein <msfeldstein@gmail.com>
2014-01-11 20:54:47 -0800

    Remove all sudo's from script, as it's already priveleged

--- OSX/Media Keys Installer/install.sh
+++ OSX/Media Keys Installer/install.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 
 # Install the sway.fm media key handler native messaging client
-syslog -s -l error "1"
-# Point to the executable in the extension so it can be updated without requiring a reinstall every time.
 binaryPath="/Library/Google/Chrome/NativeMessagingHosts/mediakeys"
-syslog -s -l error "2"
 manifestJson=`cat "$1/fm.sway.mediakeys.json-template"`
 manifestJson=`echo $manifestJson | sed -e "s|%BINARY_PATH%|${binaryPath}|g"`
 mkdir -p /Library/Google/Chrome/NativeMessagingHosts
-syslog -s -l error "3"
-echo $manifestJson | sudo tee /Library/Google/Chrome/NativeMessagingHosts/fm.sway.mediakeys.json > /dev/null
-syslog -s -l error "4"
-sudo cp "$1/mediakeys" ${binaryPath}
-syslog -s -l error "$1/mediakeys"
-syslog -s -l error ${binaryPath}
+echo $manifestJson | tee /Library/Google/Chrome/NativeMessagingHosts/fm.sway.mediakeys.json > /dev/null
+cp "$1/mediakeys" ${binaryPath}
