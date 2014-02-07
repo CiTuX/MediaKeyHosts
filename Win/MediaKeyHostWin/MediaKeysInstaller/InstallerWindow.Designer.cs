32fbaedd1b4e22787e0e49eaf602a6d13282de4d
Michael Feldstein <msfeldstein@gmail.com>
2014-02-07 05:58:57 -0800

    Create an installer for the windows host

---
+++ Win/MediaKeyHostWin/MediaKeysInstaller/InstallerWindow.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class InstallerWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InstallerWindow));
+            this.installButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // installButton
+            // 
+            this.installButton.Location = new System.Drawing.Point(314, 129);
+            this.installButton.Name = "installButton";
+            this.installButton.Size = new System.Drawing.Size(75, 23);
+            this.installButton.TabIndex = 0;
+            this.installButton.Text = "Install";
+            this.installButton.UseVisualStyleBackColor = true;
+            this.installButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(311, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Install the Media Keys Plugin";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(272, 266);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InstallerWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.installButton);
+            this.Name = "InstallerWindow";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button installButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Win/MediaKeyHostWin/MediaKeysInstaller/InstallerWindow.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.Win32;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class InstallerWindow : Form
+    {
+        public InstallerWindow()
+        {
+            InitializeComponent();
+        }
+        bool installed = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (installed)
+            {
+                Application.Exit();
+                return;
+            }
+
+            string folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Swayfm");
+            System.IO.Directory.CreateDirectory(folder);
+            CopyFile("MediaKeyHostWin.exe", folder);
+            CopyFile("fm.sway.mediakeys.json", folder);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software\\Google\\Chrome\\NativeMessagingHosts\\fm.sway.mediakeys", true);
+            key.SetValue("", Path.Combine(folder, "fm.sway.mediakeys.json"));
+            label1.Text = "You're all set!";
+            installButton.Text = "Quit";
+            installed = true;
+        }
+
+        public static void CopyFile(string filename, string folder)
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            string executablePath = Path.Combine(folder, filename);
+            string resource = @"MediaKeysInstaller.Payload." + filename;
+            using (Stream executableStream = assembly.GetManifestResourceStream(resource))
+            using (Stream output = File.Create(executablePath))
+            {
+                CopyStream(executableStream, output);
+            }
+        }
+
+        public static void CopyStream(Stream input, Stream output)
+        {
+            // Insert null checking here for production
+            byte[] buffer = new byte[8192];
+
+            int bytesRead;
+            while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, bytesRead);
+            }
+        }
+    }
+
+
+}
